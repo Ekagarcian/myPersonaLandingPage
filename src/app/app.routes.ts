f12713a83f9558de775bf879691f219ca5b93a68
Ekagarcian <156067063+Ekagarcian@users.noreply.github.com>
2024-03-17 22:42:03 +0100

    cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ServicesComponent } from './components/services/services.component';
+import { FooterComponent } from './components/footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path:'home', component: HeaderComponent},
+    {path:'about', component: AboutComponent},
+    {path:'clients', component: ClientsComponent},
+    {path:'pricing', component: PricingComponent},
+    {path:'testimonials', component: TestimonialsComponent},
+    {path:'gallery', component: GalleryComponent},
+    {path:'services', component: ServicesComponent},
+];
