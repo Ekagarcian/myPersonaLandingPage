02b5f227818665de5bdfbeff1f54240ab3d7b635
Ekagarcian <156067063+Ekagarcian@users.noreply.github.com>
2024-03-20 14:44:43 +0100

    Import ConfigItem

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
+import { ConfigItem} from './config-item';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConfigService {
 
-  configuration = [
+configuration : ConfigItem[] = [...
+];
+
     {
       id:1,
       name:"about",
