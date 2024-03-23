2449c3ebd51ac058471cdd1c952d47d3db863e9c
Ekagarcian <156067063+Ekagarcian@users.noreply.github.com>
2024-03-23 21:09:18 +0100

    se crea el componente services y la interface ConfigItem

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -2,7 +2,7 @@ import { Component, inject } from '@angular/core';
 import { ConfigItem } from '../../services/config-item';
 import { ConfigService } from '../../services/config.service';
 import { SocialComponent } from '../social/social.component';
-import { SocialComponent_1 as SocialComponent } from "../../social/social.component";
+import { SocialComponent_1 as SocialComponent } from "../social/social.component";
 
 @Component({
     selector: 'app-footer',
