ed6020bd3fdee58d558ce645535286d166302fb7
Ekagarcian <156067063+Ekagarcian@users.noreply.github.com>
2024-03-23 21:12:49 +0100

    export social

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -2,14 +2,13 @@ import { Component, inject } from '@angular/core';
 import { ConfigItem } from '../../services/config-item';
 import { ConfigService } from '../../services/config.service';
 import { SocialComponent } from '../social/social.component';
-import { SocialComponent_1 as SocialComponent } from "../social/social.component";
 
 @Component({
-    selector: 'app-footer',
-    standalone: true,
-    templateUrl: './footer.component.html',
-    styleUrl: './footer.component.css',
-    imports: [SocialComponent, SocialComponent_1]
+  selector: 'app-footer',
+  standalone: true,
+  imports: [SocialComponent],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
 })
 export class FooterComponent {
   footer!: ConfigItem | undefined;
